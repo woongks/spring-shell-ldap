c5113678e2cfba814c0911092454b9a127b79caa
김신웅 <ksw@gimsin-ungs-MacBook-Air.local>
2025-02-05 13:31:06 +0900

    first commit

---
+++ src/main/java/site/lamesa/spring_shell_ldap/SpringShellLdapApplication.java
@@ -0,0 +1,13 @@
+package site.lamesa.spring_shell_ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellLdapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/lamesa/spring_shell_ldap/SpringShellLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lamesa.spring_shell_ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
