7f9aa8d4ed61a847c017454a3815a87474240c93
ksw <ksw@gimsin-ungs-MacBook-Air.local>
2025-02-06 15:56:47 +0900

    added BasicCommandsConfig

---
+++ src/main/java/site/lamesa/spring_shell_ldap/commands/basic/BasicCommandsConfig.java
@@ -0,0 +1,37 @@
+package site.lamesa.spring_shell_ldap.commands.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.boot.CommandRegistrationCustomizer;
+import org.springframework.shell.command.CommandRegistration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class BasicCommandsConfig {
+
+    // This will apply the "basic" group to all commands
+    @Bean
+    public CommandRegistrationCustomizer basicCommandGroupCustomizer() {
+        return builder -> builder.group("basic");
+    }
+
+    // Define individual commands
+    @Bean
+    public CommandRegistration echoCommand(CommandRegistration.BuilderSupplier builder) {
+        return builder.get()
+                .command("echo")
+                .description("print what you have given as an argument")
+                .withTarget()
+                .consumer(ctx -> {
+                    String[] args = ctx.getRawArgs();
+                    String[] arg = Arrays.copyOfRange(args, 1, args.length);
+                    ctx.getTerminal().writer().println(String.join(" ",arg));
+
+                })
+                .and()
+                .build();
+    }
+}
+
